ca141e3b1c7718c5326fdb021bdc178d1f895211
Anderson <maxxd814@gmail.com>
2024-05-26 21:41:38 -0500

    EndPoinds Actualizados

--- routers/Users.py
+++ routers/Users.py
@@ -18,24 +18,6 @@ def get_db():
     finally:
         db.close()
         
-#este es el que actualmente estas usando 
-
-@user.get("/users/{email}",response_model=None)
-def get_users( email = EmailStr ,db: Session = Depends(get_db) )-> Any:
-    resultados = db.query(Model_DB.User).\
-        join(Model_DB.UserData, Model_DB.UserData.id == Model_DB.User.codigo_ID).\
-        filter(Model_DB.UserData.email == email).\
-        all()
-
-    if not resultados:
-        #aca cambie el mensaje no te olvides 
-        raise HTTPException(status_code=404, detail="buscando usuario por email: no existe")
-
-    return resultados
-
-#este es al que debes migrar ahora 
-#ahora puede usar la etiqueta del usuario
-
 @user.get("/users_nuevo/{email}",response_model=List[Usuario_Schema.UserBaseModel])
 def get_users_muevo( email = EmailStr ,db: Session = Depends(get_db) )-> Any:
     resultados = db.query(Model_DB.User,Model_DB.EtiquetaCarrera).\
@@ -68,21 +50,13 @@ def get_users_muevo( email = EmailStr ,db: Session = Depends(get_db) )-> Any:
     return response
 
 
-#este es el que usas altualmente 
-
-@user.get("/Post/",response_model=None)
-async def get_post(db: Session= Depends(get_db)) -> Any:
-    return db.query(Model_DB.Post).all()
-
-
-#mira depende de ti, lo puedo mejorar, seria que me esperes , o usarlo asi nomas, pero lo puedo mejorar pero me gana la hora
 @user.get("/posts_nuevo/",response_model=List[Publicaciones.PostWithCurso])
 async def get_post_nuevo(
     db: Session = Depends(get_db)
 ) -> Any:
     resultados = db.query(
         Model_DB.Post,Model_DB.EtiquetaCarrera,Model_DB.EtiquetaCurso).\
-            outerjoin(Model_DB.EtiquetasPublicacion,
+            join(Model_DB.EtiquetasPublicacion,
                 Model_DB.EtiquetasPublicacion.Comentario_ID == Model_DB.Post.id).\
             outerjoin(Model_DB.EtiquetaCarrera,
                 Model_DB.EtiquetaCarrera.id_carrera == Model_DB.EtiquetasPublicacion.etiqueta_carrera_ID).\
