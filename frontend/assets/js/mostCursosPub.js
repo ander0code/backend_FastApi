3fe44d12e528fe200e08851f4358b00cd76ab40e
Deyvi <deyvioscarzanches@gmail.com>
2024-05-30 15:54:06 -0500

    zzz

---
+++ frontend/assets/js/mostCursosPub.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const carreraSelect = document.getElementById('carrera');
+    const cicloSelect = document.getElementById('ciclo');
+    const cursoSelect = document.getElementById('curso');
+
+    function fetchCursos(carreraId, ciclo) {
+        fetch(`http://127.0.0.1:8000/curso/${carreraId}/${ciclo}`)
+            .then(response => response.json())
+            .then(data => {
+                // Limpiar las opciones anteriores
+                cursoSelect.innerHTML = '<option value="">Seleccionar curso</option>';
+
+                // Agregar las nuevas opciones
+                data.forEach(curso => {
+                    const option = document.createElement('option');
+                    option.value = curso.id;
+                    option.textContent = curso.nombre;
+                    cursoSelect.appendChild(option);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching cursos:', error);
+            });
+    }
+
+    function actualizarCursos() {
+        const carreraId = carreraSelect.value;
+        const ciclo = cicloSelect.value;
+
+        if (carreraId && ciclo) {
+            fetchCursos(carreraId, ciclo);
+        } else {
+            // Limpiar el select de cursos si no hay carrera o ciclo seleccionados
+            cursoSelect.innerHTML = '<option value="">Seleccionar curso</option>';
+        }
+    }
+
+    carreraSelect.addEventListener('change', actualizarCursos);
+    cicloSelect.addEventListener('change', actualizarCursos);
+});
