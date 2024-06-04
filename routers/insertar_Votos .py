62dd618e9b2e71d18f86c98ffd425e61f51e697b
Anderson <maxxd814@gmail.com>
2024-06-04 00:07:23 -0500

    previa al endponint insertar_votos falta completar

--- routers/insertar_Votos .py
+++ routers/insertar_Votos .py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException
 from sqlalchemy.orm import Session
 import pytz
+from sqlalchemy.exc import SQLAlchemyError
 from models import Model_DB
-from Schemas import Publicaciones
+from Schemas import votos
 from config.base_connection import SessionLocal
 from typing import Any,List
 from datetime import datetime
@@ -17,5 +18,40 @@ def get_db():
     finally:
         db.close()
 
-#tmr tengo miedo xD 
+@voto.post("/voto/{mensajeID}/{user_id}", response_model=None)
+async def create_post(mensajeID : int,user_id : int,comment: votos.VotosModel, db: Session = Depends(get_db)) -> Any:
+    try:
+        #arregla esto 
+        # post_user_tupla = db.query(Model_DB.User.nombre).filter(Model_DB.User.id == Post_id).first()
+        # post_user = post_user_tupla[0] if post_user_tupla else None
+        
+        # tz = pytz.timezone('America/Lima')
+        # fecha_actual_peru = datetime.now(tz)
+        # fecha_formateada = fecha_actual_peru.strftime('%Y-%m-%d')
+
+        # nuevo_post = Model_DB.Comment(
+        #                         publicacion_ID = Post_id,
+        #                         padre_comentario_id = None,
+        #                         puntuacion = 0, 
+        #                         texto = comment.texto ,
+        #                         userID = id_user,
+        #                         usuarioName = post_user,
+        #                         fecha_creacion = fecha_formateada
+        #                         )
+        # db.add(nuevo_post)
+        # db.commit()
+        # db.refresh(nuevo_post)
+        
+        response = votos.VotosResponde(
+                message=f"tipo_voto {0} agregado exitosamente",
+                status="success"
+            )
+        return response
+
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Error en la base de datos: " + str(e))
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Error interno del servidor: " + str(e))
 
