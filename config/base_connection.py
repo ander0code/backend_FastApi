9bb9308311879d0711432ff4201f6f77841ab0b3
andersonTtito <141769475+ander0code@users.noreply.github.com>
2024-07-22 21:10:43 -0500

    Update base_connection.py

--- config/base_connection.py
+++ config/base_connection.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql+pymysql://admin:..131412AdD676-1235SDs-Fa123213@database-ua.cho2k8q8ai9o.us-east-2.rds.amazonaws.com:3306/Uni_Autonoma')
+engine = create_engine('mysql+pymysql:colocar nueva database')
 
 conn = engine.connect()
 
